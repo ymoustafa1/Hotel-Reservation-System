cd4e10037c1dac1d14659f63975e2409fe0b3a5a
Malek <malekelkordy85-wq>
2026-04-16 22:59:14 +0200

    Staffsys2

---
+++ src/model/Admin.java
@@ -0,0 +1,43 @@
+package model;
+
+import database.HotelDatabase;
+
+public class Admin extends Staff
+{
+    public Admin(){}
+    public Admin(String name,String pass)
+    {
+        super(name,pass,Role.ADMIN);        //adding a new staff member of role admin
+    }
+    public void addRoom(Room r)
+    {
+        if(r==null)
+            throw new IllegalArgumentException("Room cannot be null");
+        //checking if new room id already exists before adding the new room (Validation)
+        for(Room s:HotelDatabase.rooms)
+        {
+            if(s.getRoomId()==r.getRoomId())
+                throw new IllegalArgumentException("Room ID already exists");
+        }
+        HotelDatabase.rooms.add(r);
+    }
+    //method for removing a room by removing any object of room that has a specific id
+    //if it was by Room the method would remove object reference not the room itself
+    public void removeRoom(int ID)
+    {
+        for(Room r:HotelDatabase.rooms)
+        {
+            if(r.getRoomId()==ID)
+            {
+                //checking if room is reserved before removing it
+                for (Reservation res : HotelDatabase.reservations) {
+                    if (res.getRoom().getRoomId() == roomId)        //getRoom() is a method that gets the reserved room....to be done by amira
+                        throw new IllegalArgumentException("Room has reservations");
+                }
+                HotelDatabase.rooms.remove(r);
+            }
+        }
+        throw new IllegalArgumentException("Room not found");
+    }
+
+}
