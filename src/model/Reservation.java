355ea0aa9008c20140b0d001d7948ffdf6496792
Amira <amiraa01>
2026-04-19 02:12:50 +0200

    amira finished getters, constructor, logic methods

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -13,19 +13,69 @@ public class Reservation {
     private LocalDate checkOutDate;
     private ReservationStatus status;
 
-    public Reservation(int id, Guest guest, Room room, LocalDate chin, LocalDate chout)
+    //constructor
+    public Reservation(Guest g, Room r, LocalDate in, LocalDate out)
     {
-        this.reservationId = id;
+        //assigning values
         this.guest = guest;
         this.room = room;
-        this.checkInDate = chin;
-        this.checkOutDate = chout;
+        this.checkInDate = in;
+        this.checkOutDate = out;
 
-        validateDates();
-
-        if (room.isAvailable(chin, chout))
+        //validating dates to ensure reservation dates are logically correct
+        if (!validateDates())
+        {
+            throw new IllegalArgumentException("Invalid dates.");
+        }
+        //defining room availability
+        if (room.isAvailable(in, out))
+        {
             this.status = ReservationStatus.PENDING;
+        }
         else
             throw new IllegalArgumentException("Room not available.");
+    }
+    //getters for data members
+    public boolean validateDates()
+    {
+            return checkInDate.isBefore(checkOutDate);
+    }
+
+    public Room getRoom()
+    {
+        return room;
+    }
+    public LocalDate getCheckInDate()
+    {
+        return checkInDate;
+    }
+    public LocalDate getCheckOutDate()
+    {
+        return checkOutDate;
+    }
+    public Guest getGuest()
+    {
+        return guest;
+    }
+    public int getReservationId()
+    {
+        return reservationId;
+    }
+    public ReservationStatus getStatus()
+    {
+        return status;
+    }
+    //logic methods for reservation status
+    public void cancel()
+    {
+        status = ReservationStatus.CANCELLED;
+    }
+    public void reserve()
+    {
+        status = ReservationStatus.RESERVED;
+    }
+    public void complete()
+    {
+        status = ReservationStatus.COMPLETED;
     }
 }
